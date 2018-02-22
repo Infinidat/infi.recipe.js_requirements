9842aedf7bc9685e1e95c2d752a24b24542a0b37
fanchi <rbelio@infinidat.com>
2018-02-22 17:56:45 +0200

    HOSTDEV-2811
    All requested functionality prvoided
    python 2.7/3.5 tested and working

--- src/infi/recipe/js_requirements/__init__.py
+++ src/infi/recipe/js_requirements/__init__.py
@@ -6,14 +6,33 @@ import os
 import re
 import errno
 
-from six import StringIO
+from six import BytesIO
 from six.moves import urllib
-
-import semver as semver
+from infi.pyutils import lazy
+from collections import defaultdict
+from semantic_version import Version, Spec
 import zc.buildout.easy_install
+import codecs
+
+
+class DependencyError(Exception):
+    """
+    Raised for dependency resolution errors
+    """
+    pass
+
+
+class RequirementMatchError(Exception):
+    """ Raised when we can't find a matching version for
+    """
+    pass
 
 
 class JSDep(object):
+    """
+    Responsible for the resolving, downloading, extraction and validation
+    of all javascript packages required by the project.
+    """
     REGISTRY = "https://registry.npmjs.org/"
     MIRROR = "https://skimdb.npmjs.com/registry/"
     DEFAULT_DIRECTORY = "parts/js/"
@@ -22,18 +41,20 @@ class JSDep(object):
         super(JSDep, self).__init__()
         self.buildout = buildout
         self.name = name
-        self.options = options
-        b_options = buildout['buildout']
-        p_options = buildout['project']
         js_options = buildout['js-requirements']
-        ver_split = re.compile("^([^<>=]+)([<>=]+)?([^<>=]+)?$")
-        self.js_requirements = [ver_split.findall(pkg)[0] for pkg in eval(js_options['javascript-packages'])]
+
+        spec_split = re.compile("^([^!<>=~^]+)([!<>=~^]+[^!<>=~^]+)?$")
+        self.spec_requirements = [spec_split.findall(pkg)[0] for pkg in eval(js_options['javascript-packages'])]
         options['js-directory'] = js_options['js-directory']
-        self.develop_requirements = js_options['develop-js-requirements']
-        options['develop-js-requirements'] = js_options['develop-js-requirements']
+        self.develop_requirements = options['develop-js-requirements'] = js_options['develop-js-requirements']
         options['develop-js-directory'] = js_options['develop-js-directory']
+        self.symlink_dir = options['symlink-to-directory'] = js_options['symlink-to-directory']
+
+        self.options = options
         self.created = options.created
         self.output_folder = options['js-directory'] or self.DEFAULT_DIRECTORY
+        self.versions_spec = defaultdict(set)
+        self.reader = codecs.getreader('utf-8')
 
     @staticmethod
     def _validate_hash(data, shasum):
@@ -44,60 +65,163 @@ class JSDep(object):
         :return: True on valid, False otherwise
         """
         from hashlib import sha1
-        return sha1(data).hexdigest() == shasum
+        digest = sha1(data).hexdigest()
+        if digest == shasum:
+            return True
+        else:
+            print('Invalid shasum, got: {}  , expected: {}'.format(digest, shasum))
+            return False
 
+    @lazy.cached_method
     def _get_metadata(self, pkg_name, ver=None):
         """
         Gets the JSON metadata object from the class specified REGISTRY
-        :param pkg_name: The package name to query about
-        :param ver: if provided will only get metadata for specified version, else will retrieve metadata for all versions
-        :return: Object with all the metadata values
+        :param pkg_name: str The package name to query about
+        :param ver: semantic_version.Version if provided will only get metadata for specified version,
+                    else will retrieve general metadata
+        :return: Dict
         """
         if ver:
-            url = urllib.parse.urljoin(self.REGISTRY, '/'.join([pkg_name, ver]))
+            url = urllib.parse.urljoin(self.REGISTRY, '/'.join([pkg_name, str(ver)]))
         else:
             url = urllib.parse.urljoin(self.REGISTRY, pkg_name)
-        request = urllib.request.urlopen(url)
-        pkg_metadata = json.loads(request.read())
+        response = urllib.request.urlopen(url)
+        pkg_metadata = json.load(self.reader(response))
         return pkg_metadata
 
-    def _download_package(self, pkg_metadata, version_info, validate=True):
+    def _resolve_dependencies(self):
         """
-        Downloads package with version from the class specified REGISTRY
-        :param pkg_metadata: The package name to download
-        :param version_info: the version in semver dict format
-        :param validate: If true performs shasum validation on the file downloaded, default=True
+        Resolves the dependencies according to the specified constraints. Starts with the dependencies provided
+        from the buildout.cfg and continue resolving further package dependencies in a BFS manner.
+        :return: Dict(package_name:str = selected_version:semantic_version.Version)
+        """
+        # Initialization of the BFS
+        resolved = dict()
+        bfs_stack = list()
+        for requirement_name, spec_str in self.spec_requirements:
+            self._add_spec(requirement_name, spec_str)
+            bfs_stack.append(requirement_name)
+
+        # Main loop
+        while bfs_stack:
+            # Stack Unwind
+            requirement_name = bfs_stack.pop(0)
+            available_versions = self._get_available_versions(requirement_name)
+            spec = self._get_spec(requirement_name)
+            matching_version = spec.select(available_versions)
+            if matching_version is None:
+                msg = 'Unmatched dependency for {}\nSpecification requirement: {}\nAvailable versions: {}'
+                error = msg.format(requirement_name, spec, ', '.join(reversed(map(str, available_versions))))
+                raise RequirementMatchError(error)
+
+            resolved[requirement_name] = matching_version
+
+            # Stack population
+            dependencies = self._get_dependencies(requirement_name, matching_version)
+            for dependency, dep_ver in dependencies.items():
+                self._add_spec(dependency, dep_ver)
+                bfs_stack.append(dependency)
+
+        return resolved
+
+    def _add_spec(self, requirement_name, spec_str):
+        """
+        Adds a version specification (constraint) to the set of constraints for each package requirement
+        :param requirement_name: The package name as string
+        :param spec_str: semantic_version.Version constraint as string (e.g. >=1.1.0, ~2.3.0, ^3.4.5-pre.2+build.4)
+        """
+        spec_str = spec_str or '>=0.0.0'
+        self.versions_spec[requirement_name].add(spec_str)
+
+    def _get_spec(self, requirement_name):
+        """
+        Creates a version range specification from the set of constraints for the required package name.
+        :param requirement_name: The package name as string
+        :return: semantic_version.Spec(all version specification)
+        """
+        return Spec(','.join(self.versions_spec[requirement_name]))
+
+    def _download_package(self, pkg_metadata, validate=True):
+        """
+        Downloads specified package using the NPM REGISTRY
+        :param pkg_metadata: Metadata object
+        :param validate: If true performs shasum validation on the file downloaded. default=True
         :return: True on success, false otherwise
         """
         pkg_name = pkg_metadata.get('name')
         dist = pkg_metadata.get('dist')
         tar_url = dist.get('tarball')
         shasum = dist.get('shasum')
+
+        print('\tDownloading {} from {}'.format(pkg_name, tar_url))
+
         tar_data = urllib.request.urlopen(tar_url)
-        compressed_file = StringIO(tar_data.read())
+        compressed_file = BytesIO(tar_data.read())
         if validate and not self._validate_hash(compressed_file.read(), shasum):
             return None
+
         compressed_file.seek(0)
-        tar = tarfile.open(fileobj=compressed_file, mode='r:gz')
-        package_folder = os.path.join(self.output_folder, pkg_name)
-        tar.extractall(self.output_folder)
-        self.created(package_folder)
-        os.rename(os.path.join(self.output_folder, 'package'), package_folder)
-        tar.close()
+        with tarfile.open(fileobj=compressed_file, mode='r:gz') as tar:
+            package_folder = os.path.join(self.output_folder, pkg_name)
+            tar.extractall(self.output_folder)
+        if os.path.isdir(os.path.join(self.output_folder, 'package')):
+            self.created(package_folder)
+            os.rename(os.path.join(self.output_folder, 'package'), package_folder)
+            if self.symlink_dir and 'main' in pkg_metadata:
+                self._create_symlink(package_folder, pkg_metadata['main'])
+
+    def _create_symlink(self, source_path, main):
+        """
+        Wrapper method for creating a correct symlink (or windows/ntfs link) to the main file
+        :param source_path: str
+        :param main: str
+        """
+        main_file = os.path.realpath(os.path.join(source_path, main))
+        if not os.path.isfile(main_file):
+            main_file += '.js'
+        main_file_name = os.path.basename(main_file)
+        with ChangeDirectory(os.path.realpath(self.symlink_dir)) as cd:
+            file_path = os.path.join(cd.current, main_file_name)
+            self.created(file_path)
+            if os.path.islink(file_path):
+                os.remove(file_path)
+            symlink(main_file, main_file_name)
+
+    @lazy.cached_method
+    def _get_available_versions(self, requirement_name):
+        """
+        Retrieves a sorted list of all available versions for the require package
+        :param requirement_name: str
+        :return: List(semantic_version.Version)
+        """
+        return sorted(map(Version, self._get_metadata(requirement_name).get('versions', dict()).keys()))
+
+    @lazy.cached_method
+    def _get_dependencies(self, requirement_name, version):
+        """
+        Retrieves all of the package dependencies of a specific package and version and returns a dictionary of
+        package dependency name and the spec str (e.g. >=3.1.1)
+        :param requirement_name: str
+        :param version: semantic_version.Version
+        :return: Dict(pkg_name:str = spec:str)
+        """
+        return self._get_metadata(requirement_name, version).get('dependencies', dict())
 
     def _setup(self):
-        # TODO(fanchi) - 18/Feb/2018: Parse package names and versions
+        """
+        Main function to be run by buildout
+        :return: List(all paths/files created:str)
+        """
         mkdir_p(self.output_folder)
-
-        for pkg_name, order, ver in self.js_requirements:
-            pkg_metadata = self._get_metadata(pkg_name)
-            if not ver or ver == "latest":
-                dist_tags = pkg_metadata.get('dist-tags')
-                ver = dist_tags.get('latest', '0.0')
-            version_info = semver.parse_version_info(ver)
-            # TODO(fanchi) - 18/Feb/2018: Here should be the dependency parsing
-            version_metadata = pkg_metadata.get('versions').get(ver)
-            self._download_package(version_metadata, version_info)
+        if self.symlink_dir:
+            mkdir_p(self.symlink_dir)
+        selected_versions = self._resolve_dependencies()
+        print('\n\nVersions Selected for downloading:\n')
+        print('\t' + '\n\t'.join(['{}: {}'.format(req, ver) for req, ver in selected_versions.items()]))
+        print('\n\nStarting Download:\n')
+        for pkg_name, version in selected_versions.items():
+            pkg_metadata = self._get_metadata(pkg_name, version)
+            self._download_package(pkg_metadata)
 
         return self.options.created()
 
@@ -121,29 +245,91 @@ def get_bool(options, name, default=False):
             "Invalid value for %s option: %s" % (name, value))
 
 
-def _get_matching_dist_in_location(dist, location):
-    """
-    Check if `locations` contain only the one intended dist.
-    Return the dist with metadata in the new location.
-    """
-    # Getting the dist from the environment causes the
-    # distribution meta data to be read.  Cloning isn't
-    # good enough.
-    import pkg_resources
-    env = pkg_resources.Environment([location])
-    dists = [d for project_name in env for d in env[project_name]]
-    dist_infos = [(d.project_name, d.version) for d in dists]
-    if dist_infos == [(dist.project_name, dist.version)]:
-        return dists.pop()
-    if dist_infos == [(dist.project_name.lower(), dist.version)]:
-        return dists.pop()
-
-
 def mkdir_p(path):
     try:
         os.makedirs(path)
-    except OSError as exc:  # Python >2.5
+    except OSError as exc:
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
         else:
             raise
+
+
+class ChangeDirectory(object):
+    """
+    ChangeDirectory is a context manager that allowing
+    you to temporary change the working directory.
+
+    >>> import tempfile
+    >>> td = os.path.realpath(tempfile.mkdtemp())
+    >>> currentdirectory = os.getcwd()
+    >>> with ChangeDirectory(td) as cd:
+    ...     assert cd.current == td
+    ...     assert os.getcwd() == td
+    ...     assert cd.previous == currentdirectory
+    ...     assert os.path.normpath(os.path.join(cd.current, cd.relative)) == cd.previous
+    ...
+    >>> assert os.getcwd() == currentdirectory
+    >>> with ChangeDirectory(td) as cd:
+    ...     os.mkdir('foo')
+    ...     with ChangeDirectory('foo') as cd2:
+    ...         assert cd2.previous == cd.current
+    ...         assert cd2.relative == '..'
+    ...         assert os.getcwd() == os.path.join(td, 'foo')
+    ...     assert os.getcwd() == td
+    ...     assert cd.current == td
+    ...     os.rmdir('foo')
+    ...
+    >>> os.rmdir(td)
+    >>> with ChangeDirectory('.') as cd:
+    ...     assert cd.current == currentdirectory
+    ...     assert cd.current == cd.previous
+    ...     assert cd.relative == '.'
+    """
+
+    def __init__(self, directory):
+        self._dir = directory
+        self._cwd = os.getcwd()
+        self._pwd = self._cwd
+
+    @property
+    def current(self):
+        return self._cwd
+
+    @property
+    def previous(self):
+        return self._pwd
+
+    @property
+    def relative(self):
+        c = self._cwd.split(os.path.sep)
+        p = self._pwd.split(os.path.sep)
+        ll = min(len(c), len(p))
+        i = 0
+        while i < ll and c[i] == p[i]:
+            i += 1
+        return os.path.normpath(os.path.join(*(['.'] + (['..'] * (len(c) - i)) + p[i:])))
+
+    def __enter__(self):
+        self._pwd = self._cwd
+        os.chdir(self._dir)
+        self._cwd = os.getcwd()
+        return self
+
+    def __exit__(self, *args):
+        os.chdir(self._pwd)
+        self._cwd = self._pwd
+
+
+def symlink(source, link_name):
+    os_symlink = getattr(os, "symlink", None)
+    if callable(os_symlink):
+        os_symlink(source, link_name)
+    else:
+        import ctypes
+        csl = ctypes.windll.kernel32.CreateSymbolicLinkW
+        csl.argtypes = (ctypes.c_wchar_p, ctypes.c_wchar_p, ctypes.c_uint32)
+        csl.restype = ctypes.c_ubyte
+        flags = 1 if os.path.isdir(source) else 0
+        if csl(link_name, source, flags) == 0:
+            raise ctypes.WinError()
